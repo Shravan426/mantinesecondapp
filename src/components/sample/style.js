2ebe5272385e5cff785f6c397177ad68044109bb
Shravan <shravansuresh78@gmail.com>
2024-08-26 09:22:40 +0530

    feat:"Header completed"

---
+++ src/components/sample/style.js
@@ -0,0 +1,10 @@
+import {  createStyles } from "@mantine/core";
+const useStyles = createStyles(() => ({
+//  container {
+//   BackgroundImage:URL("mantinetwo/images/Group 3.png");
+//  }
+//  Header {
+//     align-items:
+//  }
+}));
+export default useStyles;
